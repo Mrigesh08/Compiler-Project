75c868b82a99b3218f22d9e942bf888413a26f63
Mrigesh08 <mrigeshmadaan@gmail.com>
2018-04-19 06:59:40 +0530

    Quadruples code for three address code

---
+++ quad.h
@@ -0,0 +1,19 @@
+struct _e;
+typedef struct _q{
+	char * op;
+	struct _e * arg1;
+	struct _e * arg2;
+	struct _e * arg3; // this is the result field;	
+	struct _q * next;
+}Quad;
+
+
+typedef struct _ql{
+	struct _q * first;
+	struct _ql * next;
+
+}QuadList;
+
+Quad * newQuad(char * op,struct _e * arg1, struct _e * arg2, struct _e * arg3);
+
+QuadList * newQuadListElement(Quad * q);
